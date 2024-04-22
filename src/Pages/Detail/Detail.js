af8b488a4aeed542848ef8dffc98d2d8d2d44e39
umutbas <umutbasinfo@gmal.com>
2024-04-22 20:40:53 +0300

    Detail page started

--- src/Pages/Detail/Detail.js
+++ src/Pages/Detail/Detail.js
@@ -1,11 +1,39 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, Image} from 'react-native';
 import React from 'react';
+
+import styles from './Detail.style';
 
 const Detail = ({route}) => {
   const {pokemon} = route.params;
+  /* tek type pokemonlar icin gncelleme */
   return (
     <SafeAreaView>
       <Text>{pokemon.name}</Text>
+      <Text>{pokemon.height}</Text>
+      <Text>{pokemon.weight}</Text>
+      <Text>{pokemon.id}</Text>
+      <Text>{pokemon.types[0].type.name}</Text>
+      <Text>{pokemon.types[1].type.name}</Text>
+      <Text>{pokemon.abilities[0].ability.name}</Text>
+      <Text>{pokemon.abilities[1].ability.name}</Text>
+
+      <Image
+        style={styles.image}
+        width={400}
+        height={400}
+        source={{
+          uri: pokemon.sprites.front_default,
+        }}
+      />
+
+      <View style={styles.stats}>
+        <Text style={styles.statsHeader}>Stats</Text>
+        {pokemon.stats.map(c => (
+          <Text style={styles.stat} key={c.stat.name}>
+            {c.stat.name.replace('-', ' ')}: {c.base_stat}
+          </Text>
+        ))}
+      </View>
     </SafeAreaView>
   );
 };
